3e6509f6fc616bcee46d4501e06a655c4f4560eb
rlbisbe <rober90@gmail.com>
2013-04-29 23:05:53 +0200

    sent challenge 1

--- Challenge1/Program.cs
+++ Challenge1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Challenge1
 {
@@ -10,62 +7,86 @@ namespace Challenge1
     {
         static void Main(string[] args)
         {
-            string value;
-            do
-            {
-                value = Console.ReadLine();
-                ProcessNumbers(value);
+            string total = Console.ReadLine();
 
-            } while (!string.IsNullOrEmpty(value));
+            int iTotal;
+
+            if (!int.TryParse(total, out iTotal))
+                return;
+
+            while (iTotal >= 0)
+            {
+                string budget = Console.ReadLine();
+                string value = Console.ReadLine();
+                ProcessNumbers(budget, value);
+                iTotal--;
+            }
         }
 
-        static void ProcessNumbers(string numbers)
+        static void ProcessNumbers(string budget, string numbers)
         {
+            long iBudget;
+            long remainder = 0;
+            long actions = -1;
+
+            if (!long.TryParse(budget, out iBudget))
+                return;
+
             string[] values = numbers.Split(' ');
-            List<int> intValues = new List<int>();
-            List<int> minPositions = new List<int>();
-            List<int> maxPositions = new List<int>();
-            
+            List<long> intValues = new List<long>();
+
             foreach (var item in values)
             {
-                int value;
-                if (int.TryParse(item, out value))
+                long value;
+                if (long.TryParse(item, out value))
                     intValues.Add(value);
             }
 
             if (intValues[0] < intValues[1])
             {
-                minPositions.Add(0);
+                actions = iBudget / intValues[0];
+                remainder = iBudget % intValues[0];
+                iBudget = -1;
             }
 
             for (int i = 1; i < intValues.Count - 1; i++)
             {
+                //We have a min
                 if (intValues[i - 1] >= intValues[i] && intValues[i] < intValues[i + 1])
                 {
-                    minPositions.Add(i);
+                    if (iBudget == -1)
+                        continue;
+
+                    actions = iBudget / intValues[i];
+                    remainder = iBudget % intValues[i];
+                    iBudget = -1;
+
                     continue;
                 }
 
+                //We have a max
                 if (intValues[i - 1] <= intValues[i] && intValues[i] > intValues[i + 1])
-                    maxPositions.Add(i);
+                {
+                    if (actions != -1)
+                    {
+                        iBudget = actions * intValues[i] + remainder;
+                        remainder = 0;
+                        actions = -1;
+                    }
+                }
             }
 
             if (intValues[intValues.Count - 2] < intValues[intValues.Count - 1])
             {
-                maxPositions.Add(intValues.Count - 1);
+                if (actions != -1)
+                {
+                    iBudget = actions * intValues[intValues.Count - 1] + remainder;
+                    remainder = 0;
+                    actions = -1;
+                }
             }
 
-            Console.WriteLine("Min");
-            foreach (var item in minPositions)
-            {
-                Console.WriteLine(intValues[item]);
-            }
-
-            Console.WriteLine("Max");
-            foreach (var item in maxPositions)
-            {
-                Console.WriteLine(intValues[item]);
-            }
+            Console.WriteLine(iBudget);
         }
     }
 }
