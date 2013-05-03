318479bf0d5d5bdc546f496b17dc64530c242c6d
rlbisbe <rober90@gmail.com>
2013-05-03 03:34:26 +0200

    6 is almost... (this not solved yet)

--- Challenge6/Program.cs
+++ Challenge6/Program.cs
@@ -11,133 +11,169 @@ namespace Challenge6
     {
         static void Main(string[] args)
         {
-            //string total = Console.ReadLine();
+            string total = Console.ReadLine();
 
-            //int iTotal;
+            int iTotal;
 
-            //if (!int.TryParse(total, out iTotal))
-            //    return;
+            if (!int.TryParse(total, out iTotal))
+                return;
 
-            //while (iTotal > 0)
-            //{
-            //    iTotal--;
-            //    string bounds = Console.ReadLine();
-            //    string initialPosition = Console.ReadLine();
-            //    string sTime = Console.ReadLine();
-            //    Console.ReadLine();
-            //    string board = Console.ReadLine();
-            //    int time;
+            while (iTotal > 0)
+            {
+                iTotal--;
+                string key = Console.ReadLine();
 
-            //    if (!int.TryParse(sTime, out time))
-            //        continue;
+                int rows = int.Parse(key.Split(' ')[1]);
+                List<string> board = new List<string>();
+                Encoding iso = Encoding.GetEncoding(1250);
+                Encoding utf8 = Encoding.UTF8;
+                for (int i = 0; i < rows; i++)
+                {
+                    string originalMsg = Console.ReadLine();
+                    byte[] utfBytes = iso.GetBytes(originalMsg);
+                    byte[] isoBytes = Encoding.Convert(iso,utf8,utfBytes);
+                    string msg = utf8.GetString(isoBytes);
+                    board.Add(msg.Replace("TA","."));
+                }
 
-            //    Solve(initialPosition, time, board, bounds, initialPosition);
-            //}
+                Solve(key, board.ToArray());
 
-            Solve("4 5 1 3", null);
-            Console.ReadKey();
-
+            }
         }
 
         private static void Solve(string header, string[] unparsedBoard)
         {
 
             string[] sHeader = header.Split(' ');
+            int xpos = 0;
+            int ypos = 0;
             char[,] board = new char[int.Parse(sHeader[1]), int.Parse(sHeader[0])];
             bool[,] visited = new bool[int.Parse(sHeader[1]), int.Parse(sHeader[0])];
 
-            board[0, 0] = '#'; board[0, 1] = '#'; board[0, 2] = '#'; board[0, 3] = '#';
-            board[1, 0] = '#'; board[1, 1] = 'X'; board[1, 2] = '.'; board[1, 3] = '#';
-            board[2, 0] = '#'; board[2, 1] = '.'; board[2, 2] = '.'; board[2, 3] = '#';
-            board[3, 0] = '#'; board[3, 1] = '.'; board[3, 2] = '.'; board[3, 3] = 'O';
-            board[4, 0] = '#'; board[4, 1] = '#'; board[4, 2] = '#'; board[4, 3] = '#';
-
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = unparsedBoard[i][j];
+                    if (board[i, j] == 'X')
+                    {
+                        xpos = i;
+                        ypos = j;
+                    }
+                }
+            }
 
             int totalTime = 0;
-            Console.WriteLine(FindResult(board, 1, 1, visited, 0, 0, totalTime));
+            float speed = 1 / float.Parse(sHeader[2]);
+            int reactionTime = int.Parse(sHeader[3]);
+            Console.WriteLine(Math.Round(FindResult(board, xpos, ypos, visited, 0, 0, totalTime, speed, reactionTime)));
         }
 
-        private static uint FindResult(char[,] board, int x, int y, bool[,] visited, int xSlide, int ySlide, int time)
+        private static float FindResult(char[,] board, int x, int y, bool[,] visited, int xSlide, int ySlide, int time, float speed, int reactionTime)
         {
             if (x >= board.GetLength(0) || x < 0)
-                return int.MaxValue;
+                return float.MaxValue;
 
             if (y >= board.GetLength(1) || y < 0)
-                return int.MaxValue;
+                return float.MaxValue;
 
             if (visited[x, y])
-                return int.MaxValue;
+                return float.MaxValue;
 
             if (board[x, y] == '#')
-                return uint.MaxValue;
+                return float.MaxValue;
 
             if (board[x, y] == 'O')
-                return 0;
+                return 0f;
 
             visited = (bool[,])visited.Clone();
             visited[x, y] = true;
 
-            uint result = 0;
+            float result = 0;
 
+#if !DEBUG
             for (int i = 0; i < time; i++)
             {
-                Console.Write("\t");
+                Console.Write(" ");
             }
             Console.WriteLine("(B) - {2} {0},{1} . - {3} {4}", x, y, time, xSlide, ySlide);
-
+#endif
             int mul = 1;
+            int newX = 0;
+            int newY = 0;
+            bool stopped = true;
             if (xSlide != 0 || ySlide != 0)
             {
-                bool stopped = false;
+                result = speed;
+                stopped = false;
                 while (!stopped)
                 {
-                    int newX = x + mul * xSlide;
-                    int newY = y + mul * ySlide;
+                    newX = x + mul * xSlide;
+                    newY = y + mul * ySlide;
 
                     visited[newX, newY] = true;
+                    
+#if !DEBUG
                     for (int i = 0; i < time; i++)
                     {
-                        Console.Write("\t");
+                        Console.Write(" ");
                     }
                     Console.WriteLine("(S) - {2} {0},{1} . - {3} {4} ({5}) [{6}]",
-                        newX, newY, time, xSlide, ySlide, result, 
+                        newX, newY, time, xSlide, ySlide, result,
                         board[newX, newY]);
+#endif
 
-                    if (board[newX, newY] == '#' || board[newX, newY] == 'O')
+                    if (board[newX, newY] == 'O')
+                    {
+#if !DEBUG
+                        Console.WriteLine("Found it! {0} ", result);
+#endif
+                        return result + speed;
+                    }
+                    if (board[newX, newY] == '#')
                         stopped = true;
                     else
-                    {   
-                        result += 1;
+                    {
+                        result += speed;
                         mul++;
                     }
                 }
             }
             mul--;
-            Console.WriteLine(mul);
+
+            //Debug!
+            float sum = result;
+#if !DEBUG
+            Console.WriteLine(sum);
+#endif
             x = x + mul * xSlide;
             y = y + mul * ySlide;
-            Console.WriteLine("Next pos: {0},{1}",x,y);
-            uint[] possibleResults = {
-                FindResult(board, x + 1, y, visited, 1, 0, time + 1),
-                FindResult(board, x, y + 1, visited, 0, 1, time + 1),
-                FindResult(board, x, y - 1, visited, 0, - 1, time + 1),
-                FindResult(board, x - 1, y, visited, - 1, 0, time + 1)
-        
-            };
-            //Debug!
-            uint sum = 3 + result;
-            var min = possibleResults.Min();
 
-            if (min < int.MaxValue)
+            if (stopped)
+            {
+                sum += reactionTime;
+            }
+
+            float[] results = 
+            {
+                 FindResult(board, x + 1, y, visited, 1, 0, time + 1, speed, reactionTime),
+                 FindResult(board, x, y + 1, visited, 0, 1, time + 1, speed, reactionTime),
+                 FindResult(board, x, y - 1, visited, 0, -1, time + 1, speed, reactionTime),
+                 FindResult(board, x - 1, y, visited, -1, 0, time + 1, speed, reactionTime)
+            };
+
+            float min = results.Min();
+#if !DEBUG
+            if (min < float.MaxValue)
             {
                 for (int i = 0; i < time; i++)
                 {
-                    Console.Write("\t");
+                    Console.Write(" ");
                 }
-                Console.WriteLine("(T) - pos {3}. {0} {1} . - {5} {6} -  {4} {2}", x, y, sum + min, time, board[x, y], xSlide, ySlide);
+                Console.WriteLine("(E) - {2} {0},{1} . - {3} {4} [{5}]", x, y, time, xSlide, ySlide, sum + min);
             }
-
-            return sum + min;
+#endif
+            return (float)Math.Round(sum + min);
         }
     }
 }
